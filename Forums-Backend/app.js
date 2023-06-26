e7d7d0cc843c10c6ce3f1f13730b76704adb4d65
Lee1387 <127062297+Lee1387@users.noreply.github.com>
2023-06-26 19:38:43 +0100

    Added rate limiter

--- Forums-Backend/app.js
+++ Forums-Backend/app.js
@@ -1,14 +1,23 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import rateLimit from "express-rate-limt";
 
 import { loginRouter } from "./routes/login-route.js";
 import { postsRouter } from "./routes/posts-route.js";
 import { usersRouter } from "./routes/users-route.js";
 import { commentsRouter } from "./routes/comments-route.js";
 
+const limiter = rateLimit({
+    windowsMs: 60000,
+    max: 10,
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
 const app = express();
 
+app.use(limiter);
 app.use(express.json());
 
 app.use("/api/v1/login", loginRouter);
@@ -17,7 +26,7 @@ app.use("/api/v1/users", usersRouter);
 app.use("/api/v1/comments", commentsRouter);
 app.use("*", (req, res) => {
     res.status(200);
-    res.json({ message: "Page not found" });
+    res.json({ message: "The requested resource does not exist" });
 });
 
 export { app };
