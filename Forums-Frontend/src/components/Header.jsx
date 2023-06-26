aef8116f736e4a01722f8585be02a67debb9b051
Lee1387 <127062297+Lee1387@users.noreply.github.com>
2023-06-26 23:21:37 +0100

    Created login form and modal

--- Forums-Frontend/src/components/Header.jsx
+++ Forums-Frontend/src/components/Header.jsx
@@ -1,9 +1,19 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useRef } from "react";
+import { Link, Form } from "react-router-dom";
 
 import profileImage from "../assets/images/blank-profile-picture.png";
 
 export default function Header() {
+    const loginModal = useRef();
+
+    function openLoginModal() {
+        loginModal.current.showModal();
+    }
+
+    function closeLoginModal() {
+        loginModal.current.close();
+    }
+
     return (
         <header className="header">
             <div className="heading-nav-container">
@@ -26,9 +36,42 @@ export default function Header() {
                     alt="A generic blank avatar of a mans head"
                     className="profile-image"
                 />
-                <button type="button" className="button">
+                <button
+                    type="button"
+                    className="button"
+                    onClick={openLoginModal}
+                >
                     Login
                 </button>
+                <dialog className="modal" ref={loginModal}>
+                    <Form className="login-form" method="post">
+                        <h3>Enter Credentials</h3>
+                        <label htmlFor="username">Username:</label>
+                        <input 
+                            id="username"
+                            type="text"
+                            name="username"
+                            required
+                        />
+                        <label htmlFor="password">Password:</label>
+                        <input 
+                            id="password"
+                            type="text"
+                            name="password"
+                            required 
+                        />
+                        <button type="submit" className="button">
+                            Submit
+                        </button>
+                        <button 
+                            type="button"
+                            className="button"
+                            onClick={closeLoginModal}
+                        >
+                            Close
+                        </button>
+                    </Form>
+                </dialog>
             </div>
         </header>
     );
