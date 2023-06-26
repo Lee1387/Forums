e3e4da4cdf50115ccc8614fea2a8cdea654474b3
Lee1387 <127062297+Lee1387@users.noreply.github.com>
2023-06-26 01:45:16 +0100

    Added Mongoose and initial db setup

--- Forums-Backend/index.js
+++ Forums-Backend/index.js
@@ -1,9 +1,13 @@
+import mongoose from "mongoose";
+
 import { app } from "./app.js";
 
 const PORT = process.env.PORT || 3000;
 
 async function startApp() {
     try {
+        await mongoose.connect(process.env.MONGO_DB_URI);
+        console.log("Connected to database");
         app.listen(PORT, () => {
             console.log(`App is listening on port ${PORT}`);
         });
